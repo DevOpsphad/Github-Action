2ce9799ec0a5fca1be2858efc9469894d4c1f7fd
DevOpsphad <mohiniphad78@gmail.com>
2026-03-15 22:09:35 +0530

    Comment out AccountService import in SecurityConfig
    Comment out the import statement for AccountService.

--- src/main/java/com/example/bankapp/config/SecurityConfig.java
+++ src/main/java/com/example/bankapp/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.bankapp.config;
 
-import com.example.bankapp.service.AccountService;
+// import com.example.bankapp.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
